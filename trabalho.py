ab1d33d5661ef78a22c60041c58be9db19c34954
DanielGarrido06 <78385520+DanielGarrido06@users.noreply.github.com>
2024-07-03 23:18:53 -0300

    chore: Add multiprocessing support to sorting benchmark

--- trabalho.py
+++ trabalho.py
@@ -1,7 +1,6 @@
 import random
 import time
-from multiprocessing import Pool, Manager
-from functools import partial
+import multiprocessing
 
 def bubble_sort(arr):
     n = len(arr)
@@ -40,7 +39,7 @@ def counting_sort(arr):
         count[arr[i]] += 1
     k = 0
     for i in range(max_value + 1):
-        for j in range(count[i]):
+        for _ in range(count[i]):
             arr[k] = i
             k += 1
     return arr
@@ -82,46 +81,37 @@ def nano_para_micro(num):
         result += num_str[i]
     return(result)
 
-def gen_arrays(n, k, num_arrays):
-    array_geral = []
-    for _ in range(num_arrays):
-        arr = [random.randint(0, k) for _ in range(n)]
-        array_geral.append(arr)
-    return array_geral
+def gen_array(n, k):
+    arr = [random.randint(0, k) for _ in range(n)]
+    return arr
 
-def run_benchmark(array_geral, sort_func, total_times):
+def run_benchmark(arr, sort_func, sort_func_name):
     start = time.process_time_ns()
-    for arr in array_geral:
-        sort_func(arr)
+    sort_func(arr)
     end = time.process_time_ns()
-    sort_func_name = sort_func.__name__
-    if sort_func_name not in total_times:
-        total_times[sort_func_name] = 0
-    total_times[sort_func_name] += (end - start)
+    total_time = (end - start)
+    print(f"Tempo total {sort_func_name.replace('_', ' ').capitalize()}:",
+    nano_para_micro(total_time), "μs")
 
 def main():
     n = int(input("Insira o número de elementos dos arrays: "))
     k = int(input("Insira o valor máximo dos elementos dos arrays: "))
-    num_arrays = int(input("Insira o número de arrays a serem gerados: "))
 
-    manager = Manager()
-    total_times = manager.dict()
+    arr = gen_array(n, k)
 
-    array_geral = gen_arrays(n, k, num_arrays)
-
-    if __name__ == "__main__":
-        with Pool(5) as pool:
-            pool.map(partial(run_benchmark, array_geral=array_geral.copy(), total_times=total_times),
-            [bubble_sort, selection_sort, insertion_sort, counting_sort, merge_sort])
-
-
-    print("Tempo total de execução de cada algoritmo de ordenação para a mesma bateria de",
-        num_arrays, "arrays de", n, "elementos aleatórios, variando de 0 a", k, ":", "\n"+
+    print("Tempo total de execução de cada algoritmo de ordenação para o mesmo array de",
+        n, "elementos aleatórios, variando de 0 a", k, ":", "\n",
         "As unidades de tempo são microsegundos (μs), sempre inteiros, com separação de milhares por vírgulas (,).")
-    for sort_name in total_times:
-        print(f"Tempo total {sort_name.replace('_', ' ').capitalize()}:",
-              nano_para_micro(total_times[sort_name])+"μs")
-    
+
+    processes = []
+    for sort_func in [bubble_sort, selection_sort, insertion_sort, counting_sort, merge_sort]:
+        process = multiprocessing.Process(target=run_benchmark, args=(arr.copy(), sort_func, sort_func.__name__))
+        processes.append(process)
+        process.start()
+
+    for process in processes:
+        process.join()
+
 
 if __name__ == "__main__":
     main()
